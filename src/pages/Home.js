005d5485a48997eabe625bcda1ccb52c5bda9213
Abhishek jaiswal <abhishek.jaiswal@mail.valuecoders.com>
2020-07-23 13:09:24 +0530

    materical

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { db } from "../services/firebase";
 export default function HomePage() {
   const [state, setState] = useState({
     notes: []
-    
+
   })
   const [content, setCont] = useState("");
   useEffect(() => {
@@ -15,31 +15,31 @@ export default function HomePage() {
       });
       setState({ notes: allNotes });
     });
-  },[])
+  }, [])
 
   const handleChange = (e) => {
     setCont(e.target.value)
   }
 
   const createNote = () => {
-    const uid = '002';
+    const uid = '006';
     const note_id = `note-${Date.now()}`;
     db.ref(`notes/${uid}`)
-    .set({
-      content: content,
-      note_id,
-      uid
-    })
-    .then(_ => {
-      setCont("")
-    });
+      .set({
+        content: content,
+        note_id,
+        uid
+      })
+      .then(_ => {
+        setCont("")
+      });
   }
   console.log('sttae', state);
-    return (
-      <div>
-        <section>
-          <h1>Home page</h1>
-          <div>
+  return (
+    <div>
+      <section>
+        <h1>Home page</h1>
+        <div>
           <input
             onChange={(e) => handleChange(e)}
             value={state.content}
@@ -49,15 +49,15 @@ export default function HomePage() {
           </button>
         </div>
         <div>
-      {state.notes.length > 0 && state.notes.map((note, i) =>(
-        <div key={i} style={{display: 'flex'}}>
-        <p style={{ padding: `5px 10px`}}>{note.content}</p>{` `}
-        <p style={{ padding: `5px 10px`}}>{note.note_id}</p>
+          {state.notes.length > 0 && state.notes.map((note, i) => (
+            <div key={i} style={{ display: 'flex' }}>
+              <p style={{ padding: `5px 10px` }}>{note.content}</p>{` `}
+              <p style={{ padding: `5px 10px` }}>{note.note_id}</p>
+            </div>
+          ))}
         </div>
-      ))}
-      </div>
-        </section>
-      </div>
-    )
+      </section>
+    </div>
+  )
 
 }
